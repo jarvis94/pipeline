4dcdf78b00d7ac9c88a30cf01377912d1ee1f004
jarvis94 <vineesh.vijay15@gmail.com>
2018-04-09 12:44:26 +0530

    Change relative paths

--- ghdl_scripts/run_lm_sm_gen_tb.sh
+++ ghdl_scripts/run_lm_sm_gen_tb.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
-ghdl -a lm_sm_rd_gen.vhd
-ghdl -a register.vhd
-ghdl -a register_1.vhd
-ghdl -a mux.vhd
-ghdl -a lm_sm_rd_gen_tb.vhd
+ghdl -a ../rtl/lm_sm_rd_gen.vhd
+ghdl -a ../rtl/register.vhd
+ghdl -a ../rtl/register_1.vhd
+ghdl -a ../rtl/mux.vhd
+ghdl -a ../tbs/lm_sm_rd_gen_tb.vhd
 ghdl -e lm_sm_rd_gen_tb
 ghdl -r lm_sm_rd_gen_tb --stop-time=150ns --wave=lm_sm_rd_gen_tb.ghw
 #gtkwave lm_sm_rd_gen_tb.ghw &
