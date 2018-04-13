9e9f2c95970056b73f780f9e098eaf20bf52672c
jarvis94 <23626920+jarvis94@users.noreply.github.com>
2018-04-13 14:21:49 +0530

    Add bit to check if inst is lm/sm

--- rtl/lm_sm_rd_gen.vhd
+++ rtl/lm_sm_rd_gen.vhd
@@ -2,6 +2,7 @@
 ----------------------------------------
 --inputs imm_8 8 bit --> from if/id.imm
 --		 if_id_val   --> validity of if/id pipeline reg
+--		 id_lm_sm    --> from control decoder indication of the decoded instruction being lm/sm 
 --outputs rd        --> decoded destination/source address
 --		  stall     --> to hazard detector
 --        imm_8_fb  --> corrected imm_8 for next decoding
@@ -15,7 +16,7 @@ entity lm_sm_rd_gen is
 		rd: out std_logic_vector(2 downto 0);
 		imm_8_fb: out std_logic_vector(7 downto 0);
 		stall: out std_logic;
-		if_id_val: in std_logic);
+		if_id_val,id_lm_sm: in std_logic);
 end lm_sm_rd_gen;
 
 architecture behave of lm_sm_rd_gen is
@@ -64,13 +65,13 @@ with rd_sig select
 imm_8_fb_sig <= imm_8 and decode_out;
 imm_8_fb <= imm_8_fb_sig;
 -- stall process
-stall_proc: process(imm_8_fb_sig,if_id_val)
+stall_proc: process(imm_8_fb_sig,if_id_val,id_lm_sm)
 variable fb_var: std_logic;
 begin
 	fb_var := imm_8_fb_sig(0);
 	for i in 0 to 6 loop
 		fb_var := fb_var or imm_8_fb_sig(i+1);
 	end loop;
-	stall <= fb_var and if_id_val;
+	stall <= fb_var and if_id_val and id_lm_sm;
 end process;		
 end behave;
--- tbs/lm_sm_rd_gen_tb.vhd
+++ tbs/lm_sm_rd_gen_tb.vhd
@@ -8,13 +8,13 @@ end lm_sm_rd_gen_tb;
 
 architecture lm_sm_rd_gen_arc of lm_sm_rd_gen_tb is
 
-	signal clk,mux_sel,val,rst,stall_1,stall_reg: std_logic;
+	signal clk,mux_sel,val,rst,stall_1,stall_reg,lm_sm: std_logic;
 	signal imm_81,mux_out,nxt_imm,nxt_imm_reg: std_logic_vector(7 downto 0 );
 	signal rd_1,rd_reg: std_logic_vector(2 downto 0);
 
 begin
 
-dut:entity work.lm_sm_rd_gen(behave) port map(mux_out,rd_1,nxt_imm,stall_1,val);
+dut:entity work.lm_sm_rd_gen(behave) port map(mux_out,rd_1,nxt_imm,stall_1,val,lm_sm);
 mux1: entity work.mux(behav) port map(imm_81,nxt_imm_reg,mux_out,stall_reg);
 rd_reg1: entity work.register_w(behav) generic map(3) port map(rd_1,clk,rst,rd_reg);
 stall_reg1: entity work.register_1(behav) port map(stall_1,clk,rst,stall_reg);
@@ -29,24 +29,34 @@ stimulus: process
 begin
 	rst <= '1';
 	val <= '0';
+	lm_sm <= '1';
 	imm_81 <= x"FF";
 	wait for 10 ns;
 
 	rst <= '0';
-	val <= '1'; 
+	val <= '1';
+	lm_sm <= '1';	
 	imm_81 <= x"FF";
 	wait for 80 ns;
 	
 	rst <= '0';
-	val <= '1'; 
+	val <= '1';
+	lm_sm <= '1';
 	imm_81 <= x"01";
 	wait for 30 ns;
 	
 	rst <= '0';
-	val <= '0'; 
+	val <= '0';
+	lm_sm <= '1'; 
 	imm_81 <= x"FF";
 	wait for 30 ns;
 	
+	rst <= '0';
+	val <= '1';
+	lm_sm <= '0';
+	imm_81 <= x"FF";
+	wait for 30 ns;
+
 	wait;
 end process;
 end lm_sm_rd_gen_arc;
